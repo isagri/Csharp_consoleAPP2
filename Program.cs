9fbac8dd5f1e634630deadd017a7451f6f784b05
isagri <isabelle.grillet6@orange.fr>
2018-06-29 15:35:16 +0200

    appConsoleWithLib Transport Grenoble

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TransportLinesLib;
+
+namespace ConsoleApp2_withLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variable d'environnement CurrentCulture -> en-US pour que la virgule décimale soit en point lorsqu'on convertit un integer en string (appelle à l'api Métro)
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+
+            TransportLineStop tls = new TransportLineStop();
+            tls.searchLineStop(5.70, 45.17, 1000);
+
+            Console.WriteLine("Version avec dll");
+            foreach (LineStop lStop in tls.lStops)
+            {
+                Console.WriteLine("Arret : " +lStop.id + " " + lStop.name + " - lignes : ");
+                foreach (TransportLine tl in lStop.tlines)
+                {
+                    Console.WriteLine("     " + tl.id + " " + tl.longName);
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("ConsoleApp2_withLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("ConsoleApp2_withLib")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("dbe58e0d-a162-4913-a759-3812c7eaad9c")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
